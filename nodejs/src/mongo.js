c862d4e5236a758dfe4ab56ce7cb344a49d867d8
Adam Robson <adammrr@sky.com>
2022-03-22 09:24:52 +0000

    Exposed haproxy port, added basic publisher and subscriber per node container

--- nodejs/src/mongo.js
+++ nodejs/src/mongo.js
@@ -1,6 +1,6 @@
-
 //Object data modelling library for mongo
 const mongoose = require('mongoose');
+var amqp = require('amqplib/callback_api');
 
 //Mongo db client library
 //const MongoClient  = require('mongodb');
@@ -18,17 +18,80 @@ const port = 3000
 //connection string listing the mongo servers. This is an alternative to using a load balancer. THIS SHOULD BE DISCUSSED IN YOUR ASSIGNMENT.
 const connectionString = 'mongodb://localmongo1:27017,localmongo2:27017,localmongo3:27017/notflixDB?replicaSet=rs0';
 
-setInterval(function() {
+setInterval(function () {
 
-  console.log(`Intervals are used to fire a function for the lifetime of an application.`);
+  amqp.connect('amqp://test:test@192.168.56.10', function (error0, connection) {
+
+    //if connection failed throw error
+    if (error0) {
+      throw error0;
+    }
+
+    //create a channel if connected and send hello world to the logs Q
+    connection.createChannel(function (error1, channel) {
+      if (error1) {
+        throw error1;
+      }
+      var exchange = 'logs';
+      var msg = 'Hello World!';
+
+      channel.assertExchange(exchange, 'fanout', {
+        durable: false
+      });
+
+      channel.publish(exchange, '', Buffer.from(msg));
+      console.log(" [x] Sent %s", msg);
+    });
+
+
+    //in 1/2 a second force close the connection
+    setTimeout(function () {
+      connection.close();
+    }, 500);
+  });
+
 
 }, 3000);
+
+amqp.connect('amqp://test:test@192.168.56.10', function (error0, connection) {
+  if (error0) {
+    throw error0;
+  }
+  connection.createChannel(function (error1, channel) {
+    if (error1) {
+      throw error1;
+    }
+    var exchange = 'logs';
+
+    channel.assertExchange(exchange, 'fanout', {
+      durable: false
+    });
+
+    channel.assertQueue('', {
+      exclusive: true
+    }, function (error2, q) {
+      if (error2) {
+        throw error2;
+      }
+      console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", q.queue);
+      channel.bindQueue(q.queue, exchange, '');
+
+      channel.consume(q.queue, function (msg) {
+        if (msg.content) {
+          console.log(" [x] %s", msg.content.toString());
+        }
+      }, {
+        noAck: true
+      });
+    });
+  });
+});
 
 //tell express to use the body parser. Note - This function was built into express but then moved to a seperate package.
 app.use(bodyParser.json());
 
 //connect to the cluster
-mongoose.connect(connectionString, {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect(connectionString, { useNewUrlParser: true, useUnifiedTopology: true });
 
 
 var db = mongoose.connection;
@@ -52,30 +115,30 @@ var interactionModel = mongoose.model('Interactions', interactionSchema, 'intera
 
 
 app.get('/', (req, res) => {
-  interactionModel.find({},'accountID userName titleID userAction dateTime poi toi', (err, stock) => {
-    if(err) return handleError(err);
+  interactionModel.find({}, 'accountID userName titleID userAction dateTime poi toi', (err, stock) => {
+    if (err) return handleError(err);
     res.send(JSON.stringify(stock))
-  }) 
+  })
 })
 
-app.post('/',  (req, res) => {
+app.post('/', (req, res) => {
   var interaction_instance = new interactionModel(req.body);
   interaction_instance.save(function (err) {
-  if (err) res.send('Error');
+    if (err) res.send('Error');
     res.send(JSON.stringify(req.body))
   });
 })
 
-app.put('/',  (req, res) => {
+app.put('/', (req, res) => {
   res.send('Got a PUT request at /')
 })
 
-app.delete('/',  (req, res) => {
+app.delete('/', (req, res) => {
   res.send('Got a DELETE request at /')
 })
 
 //bind the express web service to the port specified
 app.listen(port, () => {
- console.log(`Express Application listening at port ` + port)
+  console.log(`Express Application listening at port ` + port)
 })
 
--- rabbitmq/tests/pubtest.js
+++ rabbitmq/tests/pubtest.js
@@ -3,7 +3,7 @@ var amqp = require('amqplib/callback_api');
 
 //connect to the MQ cluster
 
-amqp.connect('amqp://test:test@127.0.0.1', function(error0, connection) {
+amqp.connect('amqp://test:test@127.0.0.1', function (error0, connection) {
 
     //if connection failed throw error
     if (error0) {
@@ -11,25 +11,25 @@ amqp.connect('amqp://test:test@127.0.0.1', function(error0, connection) {
     }
 
     //create a channel if connected and send hello world to the logs Q
-    connection.createChannel(function(error1, channel) {
+    connection.createChannel(function (error1, channel) {
         if (error1) {
             throw error1;
         }
         var exchange = 'logs';
-        var msg =  'Hello World!';
+        var msg = 'Hello World!';
 
         channel.assertExchange(exchange, 'fanout', {
-                durable: false
+            durable: false
         });
-        
+
         channel.publish(exchange, '', Buffer.from(msg));
         console.log(" [x] Sent %s", msg);
-     });
+    });
 
-           
-     //in 1/2 a second force close the connection
-     setTimeout(function() {
-         connection.close();
-     }, 500);
+
+    //in 1/2 a second force close the connection
+    setTimeout(function () {
+        connection.close();
+    }, 500);
 });
 
